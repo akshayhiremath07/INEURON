54a5690e1baad6e1c7cfbeffa0024673bafe1772
akshayhiremath07 <akshayhiremath623@gmail.com>
2022-10-20 13:30:52 -0700

    assignment

---
+++ getitdone/Ineuron.java
@@ -0,0 +1,104 @@
+package com.getitdone;
+
+public class Ineuron {
+
+	public static void main(String[] args) {
+	
+		 int n=10;
+		 for(int i=0;i<n;i++) {
+			 for(int j=0;j<n;j++) {
+				 if(j==(n-1)/2) {
+					 System.out.print("*");
+				 }
+				 else {
+					 System.out.print(" ");
+				 }
+			 }
+			 for(int j=0;j<n;j++) {
+				 if(i==j || j==0 ||j==(n-1)) {
+					 System.out.print("*");
+				 }
+				 else {
+					 System.out.print(" ");
+				 }
+				 
+			 }
+			 for(int j=0;j<n;j++) {
+				 if(j==1 ||j==0) {
+					 System.out.print(" ");
+				 }
+			 }
+			 for(int j=0;j<n;j++) {
+				 if(i==0 &j!=0||i==(n-1)/2|| i==(n-1)||j==0 ) {
+					 System.out.print("*");
+				 }
+				 else {
+					 System.out.print(" ");
+				 }
+				 
+			 }
+			 for(int j=0;j<n;j++) {
+				 if(j==1 ||j==0) {
+					 System.out.print(" ");
+				 }
+			 }
+			 for(int j=0;j<n;j++) {
+				 if(j==0 &&i<n-1 ||i==n-1&&j!=0 &&j!=n-1||j==n-1&&i<n-1) {
+					 System.out.print("*");
+				 }
+				 else {
+					 System.out.print(" ");
+				 }
+				 
+			 }
+			 for(int j=0;j<n;j++) {
+				 if(j==1 ||j==0 ) {
+					 System.out.print(" ");
+				 }
+			 }
+			 for(int j=0;j<n;j++) {
+				 if(j==0||i==0&&j!=n-1||j==n-1&&i!=0 &&i<(n-1)/2||i==(n-1)/2&&j!=n-1||j==n-1&&i>(n-1)/2) {
+					 System.out.print("*");
+				 }
+				 else {
+					 System.out.print(" ");
+				 }
+				 
+			 }
+			 for(int j=0;j<n;j++) {
+				 if(j==1 ||j==0 ) {
+					 System.out.print(" ");
+				 }
+			 }
+			 for(int j=0;j<n;j++) {
+				 if(j==0 && i!=0 &&i!=n-1|| i==0 && j!=0&&j!=n-1||j==n-1 &&i!=0&&i!=n-1||i==n-1&&j!=0&&j!=n-1) {
+					 System.out.print("*");
+				 }
+				 else {
+					 System.out.print(" ");
+				 }
+				 
+			 }
+			 for(int j=0;j<n;j++) {
+				 if(j==1 ||j==0 ) {
+					 System.out.print(" ");
+				 }
+			 }
+			 for(int j=0;j<n;j++) {
+				 if(i==j || j==0 ||j==(n-1)) {
+					 System.out.print("*");
+				 }
+				 else {
+					 System.out.print(" ");
+				 }
+				 
+			 }
+			 
+			 
+			 
+			 System.out.println();
+		 }
+
+	}
+
+}
